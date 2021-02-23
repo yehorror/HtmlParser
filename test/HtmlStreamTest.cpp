167f2d946e96c77e3804c79e36a7fb2c656ae5b6
yehor <eli0t1326@gmail.com>
2021-02-23 15:01:30 +0200

    Add test to check normal reading of a tag from stream

--- test/HtmlStreamTest.cpp
+++ test/HtmlStreamTest.cpp
@@ -1,2 +1,14 @@
 #include <gtest/gtest.h>
 #include "Impl/HtmlStream/HtmlStream.hpp"
+
+using namespace HtmlParser::Impl;
+
+TEST(HtmlStreamTest, TestReadingOfATagFromStream)
+{
+    const std::string HTML = "<body> whatever content that won't be parsed </body>";
+
+    HtmlStream stream(HTML);
+    const std::string parsedTag = stream.ReadTag();
+
+    EXPECT_EQ(parsedTag, "<body>");
+}
